da2a07c23580a75dabe26887a1f95a39e7db2665
Allegra Anka-Mueller <allegraanka-mueller@ip-10-97-143-217.eu-west-1.compute.internal>
2023-10-27 12:18:25 +0100

    Add more

--- aa3718-page/src/app/home/page.tsx
+++ aa3718-page/src/app/home/page.tsx
@@ -2,5 +2,10 @@ import { useState } from "react";
 
 // In React, compoenents are functions
 export default async function header() {
-  return <h1>Hello World :)</h1>;
+  return (
+    <>
+      <h1>Hello World :)</h1>
+      <p>This is under construction</p>
+    </>
+  );
 }
